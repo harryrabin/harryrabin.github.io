979f78094e6b683dc8cae3f43f35560e8967888a
harryrabin <52754948+harryrabin@users.noreply.github.com>
2021-05-09 03:44:05 -0700

    Layout changes

--- interact.js
+++ interact.js
@@ -1,7 +1,21 @@
+// Makes nav links close the navbar toggle
+
 let navLinks = document.querySelectorAll('.nav-link');
-let navCollapse = new bootstrap.Collapse(document.querySelector('#nav-collapse'), {
+let navCollapse = new bootstrap.Collapse(document.getElementById('nav-collapse'), {
   toggle: false
 });
+
 for (link of navLinks) {
-  link.addEventListener('click', () => {navCollapse.hide()});
+  link.addEventListener('click', () => {
+    navCollapse.hide()
+  });
 }
+
+// function isBreakpoint(points) {
+//   for (p of points) {
+//     if ($('.device-' + p).is(':visible')) {
+//       return true;
+//     }
+//   }
+//   return false;
+// }
